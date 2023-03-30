d4cd65fd360f14d91875208b347f748d3c11373c
yassinehaimouch <haymouch53@gmail.com>
2023-03-30 20:43:34 +0000

    Add: ProjectInput Class with autobind

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,53 @@
-// Code goes here!
+// autobind
+function Autobind(_target: any, _name: string, descriptor: PropertyDescriptor){
+  const originalMethod = descriptor.value;
+  const adjDescripor: PropertyDescriptor = {
+    configurable: true,
+    get(){
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  }
+  return adjDescripor;
+}
+
+//class ProjectInput
+class ProjectInput{
+  templeteElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor(){
+    this.templeteElement = document.getElementById("project-input")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templeteElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement; 
+    this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement; 
+    this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement; 
+    this.configure();
+    this.attach();   
+  }
+
+  @Autobind
+  private submitHandler(event: Event){
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure(){
+    this.element.addEventListener('submit', this.submitHandler)
+  }
+
+  private attach(){
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
